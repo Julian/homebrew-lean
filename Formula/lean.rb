e87b7a892fbc3395b2c0e6b6d34856367c9c2b06
Julian Berman <Julian@GrayVines.com>
2023-08-11 19:52:33 +0100

    Add workflows and bring in the Lean formula

---
+++ Formula/lean.rb
@@ -0,0 +1,36 @@
+class Lean < Formula
+  desc "Theorem Prover"
+  homepage "https://leanprover.github.io/"
+  license "Apache-2.0"
+  head "https://github.com/leanprover/lean4.git"
+
+  depends_on "cmake" => :build
+  depends_on "coreutils"
+  depends_on "gmp"
+  depends_on "jemalloc"
+  depends_on macos: :mojave
+
+  conflicts_with "elan-init", because: "it installs the same binaries"
+  conflicts_with "lean", because: "is installs the same binaries"
+
+  def install
+    mkdir "build/release" do
+      system "cmake", "../..", *std_cmake_args
+      system "make", "-j#{ENV.make_jobs}", "install"
+    end
+  end
+
+  test do
+    (testpath/"hello.lean").write <<~EOS
+      def id' {α : Type} (x : α) : α := x
+
+      example (a b : Prop) : a ∧ b -> b ∧ a :=
+      by
+        intro h
+        cases h
+        exact ⟨by assumption, by assumption⟩
+    EOS
+    system bin/"lean", testpath/"hello.lean"
+    system bin/"lake", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Lean Homebrew Tap
+
+## How do I install this?
+
+`brew install julian/lean/mathlib4`
+
+Though you should have a reason you're not using the official builds.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
