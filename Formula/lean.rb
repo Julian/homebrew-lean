67eed8ba565c805ce8682f34a81d9ac8fe75848f
Julian <Julian@users.noreply.github.com>
2024-12-02 11:06:53 +0000

    Update to the new Mathlib4 Lean version

--- Formula/lean.rb
+++ Formula/lean.rb
@@ -1,8 +1,8 @@
 class Lean < Formula
   desc "Theorem Prover"
   homepage "https://leanprover.github.io/"
-  url "https://github.com/leanprover/lean4/archive/refs/tags/v4.14.0-rc3.tar.gz"
-  sha256 "86f5d6556020d24b8f23e094104d7de9386139c3d6c145e4186a111eb1a90867"
+  url "https://github.com/leanprover/lean4/archive/refs/tags/v4.15.0-rc1.tar.gz"
+  sha256 "1be413e4fc8af2c64a097398c529a1ca136cdfd312a088d61a544706d91dbd4d"
   license "Apache-2.0"
   head "https://github.com/leanprover/lean4.git"
 
